911a2a34d1065ff17ec6ff3b3602ed987f1dcb17
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2023-01-10 20:55:33 +0000

    Add StepProgress

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import StepProgress from "./Step/StepProgress";
+
+const Main = () => {
+  return (
+    <main class="site-main">
+      <div class="main-container">
+     <StepProgress />
+      </div>
+    </main>
+  )
+};
+
+export default Main;
